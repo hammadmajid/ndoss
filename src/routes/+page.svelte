6b835f96b8fa47ac1295c84283fa68a9079c7c6e
Hammad Majid <hammadmajid@proton.me>
2024-09-17 13:40:37 +0500

    add basic hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<main class="space-y-6 px-4">
+  <div class="space-y-2">
+    <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      NDOSS
+    </h1>
+    <p class="leading-7 text-muted-foreground [&:not(:first-child)]:mt-6">
+      Niche Document OCR Scanner and Sorter
+    </p>
+  </div>
+
+  <Button>Upload file</Button>
+</main>
