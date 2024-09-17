81cfe2820563791d0c44b8b25f8ad58a7ba66969
Hammad Majid <hammadmajid@proton.me>
2024-09-17 09:02:59 +0000

    refactor root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 
 <ModeWatcher />
 
-<div class="min-h-screen">
+<svelte:head>
+  <title>NDOSS</title>
+</svelte:head>
+
+<div class="min-h-screen md:w-2/3 lg:w-1/2 mx-auto">
   <slot></slot>
 </div>
